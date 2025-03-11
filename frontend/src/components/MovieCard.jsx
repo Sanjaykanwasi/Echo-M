e7bb5a06b5d76c6f325d28f4696cbedf67a236d4
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-03-11 23:43:18 +0530

    key1

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -4,13 +4,15 @@ import "../css/MovieCard.css";
 const MovieCard = ({ movie }) => {
   // Function
   const onFavouriteClick = (e) => {
-    e.preventDefault();
     alert("🤍");
   };
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.img} alt={movie.title} />
+        <img
+          src={`https://image.tmbd.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favourite-btn" onClick={onFavouriteClick}>
             🤍
@@ -19,7 +21,7 @@ const MovieCard = ({ movie }) => {
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
   );
