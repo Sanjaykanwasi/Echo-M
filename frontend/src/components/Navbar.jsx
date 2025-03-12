ae7b6460326c188cbde9456b4e6642ef4b134345
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-03-12 12:13:17 +0530

    color

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,13 +5,13 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
+        <Link className="!text-white" to="/">Movie App</Link>
       </div>
       <div className="navbar-links">
-        <Link to="/" className="nav-link">
+        <Link to="/" className="nav-link !text-white">
           Home
         </Link>
-        <Link to="/favorites" className="nav-link">
+        <Link to="/favorites" className="nav-link !text-white">
           Favorites
         </Link>
       </div>
